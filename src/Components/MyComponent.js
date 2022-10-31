9a7a80e7648a4d89c4804c2e57d8a3a0d7a6d94b
NgQuocThang3k <bossofwold2000@gmail.com>
2022-10-31 16:08:08 +0700

    hoc DOM events

--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -5,10 +5,18 @@ class MyComponent extends React.Component {
         name: " Thang",
         nick: " dep trai vai lon",
         state:"thieu tien cho gai",
+    };
+     handleClick (event){
+       // console.log(">> click me my button");
+       console.log(event.target);
     }
     render(){
         return(
-            <div> my nick Name is {this.state.nick+ this.state.name} </div>
+            <div>
+                 my nick Name is {this.state.nick+ this.state.name} dang {this.state.state}
+                 <button onClick={this.handleClick}> Click me </button>
+            </div>
+           
         );
     }
 }
